cfc445758c21ac6ba7080f8b87e31e17b2be5251
InfoMarcy <30274064+InfoMarcy@users.noreply.github.com>
2021-02-16 19:13:39 -0600

    index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once "controladores/general.controlador.php";
 require_once 'controladores/paypal.controlador.php';
 require_once 'controladores/email.controlador.php';
 require_once 'vendor/phpmailer/phpmailer/autoload.php';
-// require_once 'PHPMailer/PHPMailerAutoload.php';
+// require_once 'PHPMailer/PHPMailerAutoload.php'; dfsfdsf
 
 $plantilla =  new ControladorPlantilla();
 $plantilla->ctrPlantilla();
