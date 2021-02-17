0b56a21a13030c6de78e261b8ceaaa9d7e7f2b4c
InfoMarcy <30274064+InfoMarcy@users.noreply.github.com>
2021-02-16 18:58:46 -0600

    se agrega php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require_once 'controladores/plantilla.controlador.php';
 require_once "controladores/general.controlador.php";
 require_once 'controladores/paypal.controlador.php';
+require_once 'controladores/email.controlador.php';
+require_once 'PHPMailer/PHPMailerAutoload.php';
 
 $plantilla =  new ControladorPlantilla();
 $plantilla->ctrPlantilla();
